4e63ac498e276fd6f58aeec8facb6ff585e2e998
felipesalesoliveira <fesales.oliveira@gmail.com>
2026-08-11 20:47:20 -0300

    Fix asset paths for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://felipesalesdeoliveira.github.io',
-  base: '/felipe-sales-technical-portfolio',
+  base: '/felipe-sales-technical-portfolio/',
 });
